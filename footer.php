9fc4a3dfd234897145ea8044ce0e18a29c1820ab
helewei <Lewei.He@linde-china.com>
2018-04-19 09:15:40 +0800

    optimize code.

--- footer.php
+++ footer.php
@@ -1,39 +1,43 @@
 <?php $current_options = wp_parse_args(get_option('busiprof_theme_options', array()), theme_setup_data()); ?>
 <!-- Footer Section -->
-<footer class="footer-sidebar">	
-	<!-- Footer Widgets -->	
-	<div class="container">		
-		<div class="row">		
-			<?php if ( is_active_sidebar( 'footer-widget-area' ) )
-					{ 
-						dynamic_sidebar( 'footer-widget-area' );  
-					} 
-			?>
-		</div>
-	</div>
-	<!-- /End of Footer Widgets -->	
-	
-	<!-- Copyrights -->	
-	<div class="site-info">
-		<div class="container">
-			<div class="row">
-				
-				<div class="col-md-3">
-					<p>
-						<a href="http://www.miitbeian.gov.cn/" target="_blank">闽ICP备18008699</a>
-					</p>
-				</div>
-				
-				<div class="col-md-7">
-				<?php 
-				if( $current_options['footer_copyright_text'] != '' ) { ?>
-				<?php echo $current_options['footer_copyright_text']; ?>
-				<?php } ?>
-				</div>
-			</div>
-		</div>
-	</div>
-	<!-- Copyrights -->	
+<footer class="footer-sidebar">
+    <!-- Footer Widgets -->
+    <div class="container">
+        <div class="row">
+            <?php if (is_active_sidebar('footer-widget-area')) {
+                dynamic_sidebar('footer-widget-area');
+            }
+            ?>
+        </div>
+    </div>
+    <!-- /End of Footer Widgets -->
+
+    <!-- Copyrights -->
+    <div class="site-info">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-3">
+                    <p>
+                        <a href="http://www.miitbeian.gov.cn/" target="_blank">闽ICP备18008699号</a>
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <p>
+                        |
+                    </p>
+                </div>
+
+                <div class="col-md-7">
+                    <p>
+                        版权所有：厦门本物载体软件技术有限公司
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Copyrights -->
 
 </footer>
 <!-- /End of Footer Section -->
